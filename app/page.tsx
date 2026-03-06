31149ca6a1c69ce7170baebece3f6d35911e8a02
yashsharmadeve <yashsharmasbm@gmail.com>
2026-03-07 00:24:46 +0530

    Change heading

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
       <div className="my-2">
         <h1 className="text-3xl font-bold underline text-center">Welcome to My Blog</h1>
-            <Link href={'/blog'} className="mt-3 text-blue-600 underline text-lg text-center block">Go to Blogs</Link>
+            <Link href={'/blog'} className="mt-3 text-blue-600 underline text-lg text-center block">Go to Blog</Link>
       </div>
   );
 }
